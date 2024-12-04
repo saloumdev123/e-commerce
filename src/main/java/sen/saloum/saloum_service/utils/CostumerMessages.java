2a8a4f70b9c10e4a5b31772621759ec27221576c
saloumdev123 <sensaloumdev@gmail.com>
2024-12-04 13:30:38 +0100

    getting in tuch with customer messages

---
+++ src/main/java/sen/saloum/saloum_service/utils/CostumerMessages.java
@@ -0,0 +1,16 @@
+package sen.saloum.saloum_service.utils;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CostumerMessages {
+
+    public static String DELETE_USER = "Utilisateur supprime avec succes";
+    public static String USER_DELETION_FAILED = "Echec de la suppression de l'utilisateur";
+    public static String FETCH_USERS_SUCCESS = "Utilisateurs récupérés avec succès.";
+    public static String FETCH_USER_SUCCESS = "Utilisateur récupéré avec succès.";
+    public static String USER_NOT_FOUND = "Utilisateur non trouvé.";
+    public static String USER_CREATED = "Utilisateur créé avec succès.";
+    public static String USER_CREATION_FAILED = "Échec de la création de l'utilisateur.";
+
+}
