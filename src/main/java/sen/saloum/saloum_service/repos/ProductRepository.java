34069b725eb14d21acec1e5002f8cc0664f53117
saloumdev123 <sensaloumdev@gmail.com>
2024-12-04 13:32:52 +0100

    adding method in product repository to personnalize the requte

--- src/main/java/sen/saloum/saloum_service/repos/ProductRepository.java
+++ src/main/java/sen/saloum/saloum_service/repos/ProductRepository.java
@@ -3,5 +3,8 @@ package sen.saloum.saloum_service.repos;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sen.saloum.saloum_service.domain.Product;
 
+import java.util.Optional;
+
 public interface ProductRepository  extends JpaRepository<Product, Long> {
+    Optional<Product> findByNom(String productName);
 }
