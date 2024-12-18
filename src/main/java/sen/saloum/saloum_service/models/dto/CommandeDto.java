3db17cd7f72ee4be332474060806ca44ca89a6e5
saloumdev123 <sensaloumdev@gmail.com>
2024-12-18 20:05:41 +0100

    refactoring commande  dto

--- src/main/java/sen/saloum/saloum_service/models/dto/CommandeDto.java
+++ src/main/java/sen/saloum/saloum_service/models/dto/CommandeDto.java
@@ -6,16 +6,18 @@ import lombok.NoArgsConstructor;
 import sen.saloum.saloum_service.models.enums.StatutCommande;
 
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CommandeDto {
+
     private Long id;
-    private LocalDateTime dateCommande;
+    private OffsetDateTime dateCommande;
     private Double montantTotal;
     private StatutCommande statut;
-
-    private List<LigneCommandeDto> lignes; // Use standalone LigneCommandeNestedDto
+    private UtilisateurDto utilisateurDto;
+    private List<LigneCommandeDto> lignes;
 }
