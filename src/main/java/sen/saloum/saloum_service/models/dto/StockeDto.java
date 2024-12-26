734ed4b169a99f397e871c8e590a55de879bf85f
saloumdev123 <sensaloumdev@gmail.com>
2024-12-26 18:23:51 +0100

    updating the Dto by replacing the object with Long type of user

--- src/main/java/sen/saloum/saloum_service/models/dto/StockeDto.java
+++ src/main/java/sen/saloum/saloum_service/models/dto/StockeDto.java
@@ -11,5 +11,5 @@ public class StockeDto {
     private Long id;
     private Integer quantiteDisponible;
     private Integer seuilAlerte;
-    private ProductDto product;
+    private Long product;
 }
