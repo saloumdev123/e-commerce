fbdc3994d107e7083f47fdfd70f2b7645ebd7888
saloumdev123 <sensaloumdev@gmail.com>
2025-01-02 21:42:57 +0100

    handling stocke dto

--- src/main/java/sen/saloum/saloum_service/models/dto/StockeDto.java
+++ src/main/java/sen/saloum/saloum_service/models/dto/StockeDto.java
@@ -1,5 +1,7 @@
 package sen.saloum.saloum_service.models.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +11,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class StockeDto {
     private Long id;
+
+    @NotNull(message = "La quantité disponible est obligatoire")
+    @Min(value = 0, message = "La quantité disponible doit être un nombre positif")
     private Integer quantiteDisponible;
+
+    @NotNull(message = "Le seuil d'alerte est obligatoire")
+    @Min(value = 1, message = "Le seuil d'alerte doit être supérieur ou égal à 1")
     private Integer seuilAlerte;
+    @NotNull(message = "L'ID du produit est obligatoire")
     private Long product;
 }
