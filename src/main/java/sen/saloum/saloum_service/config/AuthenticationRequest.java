22f15f5292c5f87cab9c1bad1029d8a9bb960469
= <sensaloumdev@gmail.com>
2025-01-26 02:28:40 +0000

    updting the AuthenticationRequest to have the correct credential that correspond with the user credentials

--- src/main/java/sen/saloum/saloum_service/config/AuthenticationRequest.java
+++ src/main/java/sen/saloum/saloum_service/config/AuthenticationRequest.java
@@ -7,6 +7,6 @@ import lombok.Setter;
 @Setter
 public class AuthenticationRequest {
 
-    private String username;
-    private String password;
+    private String email;
+    private String motDePasse;
 }
